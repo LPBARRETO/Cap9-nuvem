13edfdd515df929cc21cfe5cd60c003ce4f97ba0
Lucas barreto <lpbarreto97@gmail.com>
2026-06-08 03:11:02 -0300

    Atualizando o app.py
    De modo que esse arquivo comporte tanto o exercicio 9.3 quanto o 9.4

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
-# A variável OBRIGATORIAMENTE precisa se chamar "app" para o Azure encontrá-la
 app = Flask(__name__)
 
+# ==========================================
+# EXERCÍCIO 9.3 - A Rota Hello World
+# ==========================================
 @app.route('/')
 def hello_cloud():
     return 'Hello, World! Meu Flask está rodando na Nuvem do Azure!'
+
+# ==========================================
+# EXERCÍCIO 9.4 - A Rota da API RESTful
+# ==========================================
+tarefas = [
+    {"id": 1, "titulo": "Finalizar o Capítulo 9", "concluida": False}
+]
+
+@app.route('/tarefas', methods=['GET'])
+def obter_tarefas():
+    return jsonify(tarefas), 200
+
+@app.route('/tarefas', methods=['POST'])
+def criar_tarefa():
+    nova_tarefa = request.get_json()
+    tarefas.append(nova_tarefa)
+    return jsonify({"mensagem": "Tarefa criada com sucesso!", "tarefa": nova_tarefa}), 201
 
 if __name__ == '__main__':
     app.run()
